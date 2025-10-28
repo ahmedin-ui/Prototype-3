2cb7a769c335c5a95c9c8c2f71b9b5a742a5fdaf
Naziya <ahmedi.n@indivillage.in>
2025-10-28 15:56:35 +0530

    Instantiate obstacle prefabs.

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
-    private Vector3 spawnPos;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
     }
 
     // Update is called once per frame
