d0db6ab185d5fcb0561fd64beae7341e48862f24
Naziya <ahmedi.n@indivillage.in>
2025-10-28 15:42:43 +0530

    Translate an obstacle to the left according to the speed variable

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -14,6 +14,6 @@ public class MoveLeft : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
     }
 }
