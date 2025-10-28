9c59aca0f00edff538f8bdad34c06d13026da818
Naziya <ahmedi.n@indivillage.in>
2025-10-28 14:09:25 +0530

    initialize the variable playerRb to rigidbody

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,8 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.AddForce(Vector3.up * 1000);
         
     }
 
